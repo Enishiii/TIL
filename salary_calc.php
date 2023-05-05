e1a5da92f395e09cbaae8417d2af51e2aa9cfb50
Enishi Yoshioka <enishiyoshioka@gmail.com>
2023-05-05 18:41:56 +0900

    手取り計算を追加

--- salary_calc.php
+++ salary_calc.php
@@ -1,7 +1,14 @@
 <?php
 
-$salary = 4500000;
+echo '初任給を入力してください。（年収）' . PHP_EOL;
+$salary = trim(fgets(STDIN));
+
+echo '昇給率を入力してください（0〜100の半角英数字）' . PHP_EOL;
+$salary_increase_rate = trim(fgets(STDIN));
+$salary_increase_rate = $salary_increase_rate / 100 + 1;
+
 for ($i = 1; $i <= 10; ++$i) {
     echo $i . '年目の給料は' . $salary . '円です。' . PHP_EOL;
-    $salary = $salary * 1.1;
+    echo '手取りは' . $salary * 0.75 . '~' . $salary * 0.85 . '円です。' . PHP_EOL;
+    $salary = $salary * $salary_increase_rate;
 }
